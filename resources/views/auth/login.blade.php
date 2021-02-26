fc85ffcaf3bd187715378f68be9b74a8c69f16b2
zohaibmalikie <zohaibmalikie@gmail.com>
2021-02-26 16:16:31 +0500

    login page routing fixed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,8 +4,6 @@
 	
 	@section('extra-css')
 
-		<link rel="stylesheet" type="text/css" href="{{ asset('assets/admin/css/pages/login.css') }}">
-
 	@endsection
 {{-- head end --}}
 
@@ -14,97 +12,102 @@
 
 	@section('content')
 		
-		<div class="row">
-	      <div class="col s12">
-	        <div class="container"><div id="login-page" class="row">
-	          <div class="col s12 m6 l4 z-depth-4 card-panel border-radius-6 login-card bg-opacity-8">
-	            <form class="login-form"  method="POST" action="{{ route('login') }}">
-	            	@csrf
-	              	<div class="row">
-		                <div class="input-field col s12 text-center">
-		                  <h5 class="ml-4">Sign in</h5>
-		                </div>
+	<div class="bg-register-img">
+      <div class="padding-top-reg main_padding">
+        <div class="row m-0 box-shadow-reg form-radius rounded-right-reg">
+          <div class="col-md-5 bg-register-img2 p-0">
+            
+              
+              <div class="text-center padding-bottom-reg-1 inner-padding">
+                <img
+                  src="{{ asset('assets/frontend/images/register_logo.png') }}"
+                  alt=""
+                  class="img-fluid "
+                />
+                <div class="display-2 font-600 text-white pt-5">Welcome Back!</div>
+              
+                <div class="h3 m-0 text-white pt-3">To keep connected with us please <br> login with your personal info</div>
+         
+              </div>
+          
+            
+          </div>
+          <div
+            class="col-md-7 padding-right-reg padding-left-reg bg-white rounded-right-reg"
+          >
+            
+            <div class="h1 text-center cl-3AC574 padding-top-reg">Signin to Learnmelive</div>
+            <div class="pl-5 pr-5">
+            
+              <div >
+                  <div class="row pt-3">
+                      <div class="col-md-12 text-center">
+                          <img src="{{ asset('assets/frontend/images/fb-icon.png') }}" alt="" class="img-fluid">
+                          <img src="{{ asset('assets/frontend/images/google.png') }}" alt="" class="img-fluid">
+                          <img src="{{ asset('assets/frontend/images/apple.png') }}" alt="" class="img-fluid">
+                      </div>
+                  </div>
+                  <div class="row pt-3">
+                      <div class="col-md-12 text-center">
+                         <h6 class="cl-gray">or use email for login</h6>
+                      </div>
+                  </div>
+              </div>
+             
+              <div class="input-group mb-3 border-input pt-4">
+                <span><img src="{{ asset('assets/frontend/images/men-8 (1).png') }}" alt="" /></span>
+                <span
+                  ><input
+                    type="text"
+                    class="form-control border-0"
+                    placeholder="Email / Username"
+                    aria-label=""
+                    aria-describedby="basic-addon1"
+                /></span>
+              </div>
+              <div class="input-group border-input pt-4 mb-5">
+                <span><img src="{{ asset('assets/frontend/images/key-8.png') }}" alt="" /></span>
+                <span
+                  ><input
+                    type="text"
+                    class="form-control border-0 "
+                    placeholder="Enter your password"
+                    aria-label=""
+                    aria-describedby="basic-addon1"
+                /></span>
+              </div>
+              <button
+                type="button"
+                class="btn bg-3AC574 w-100 mt-2 pt-2 pb-2 mb-3 text-white"
+              >
+                Sign in
+              </button>
+              <div class="row pt-4 pb-4 cl-gray p-0 m-0 border-bottom">
+               <div class="col-md-6 text-left">
+                    <div class="form-check pl-0">
+                        <input class="form-check-input border-3AC574" type="checkbox" value="" id="defaultCheck1">
+                        <label class="form-check-label cl-3AC574 font-600 pl-2" for="defaultCheck1">
+                         Remember me
+                        </label>
+                    </div> 
+                </div>
+                <div class="col-md-6 text-right">
 
-		                <div class="input-field col s12">
-		                	@if (session('message'))
+                    <h6 class="cl-3AC574">Forgotten password</h6>
+                </div>
+              </div>
+              <div class="text-center">
+                <h6 class="m-0 mt-4 cl-gray">
+                  Not a member yet? 
+                  <span class="cl-3AC574"> Join now</span>
+                </h6>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
-		                		<div class="card-alert card gradient-45deg-red-pink">
-					                <div class="card-content white-text">
-					                  <p>
-					                    <i class="material-icons">error</i> {{ session('message') }}</p>
-					                </div>
-					               {{--  <button type="button" class="close white-text" data-dismiss="alert" aria-label="Close">
-					                  <span aria-hidden="true">×</span>
-					                </button> --}}
-					             </div>
-						    @endif
-		                </div>
-	              	</div>
-
-	                <div class="row margin">
-		                <div class="input-field col s12">
-		                  <i class="material-icons prefix pt-2">person_outline</i>
-		                  <input id="username" type="text" name="email" value="{{ old('email') }}">
-		                  <label for="username" class="center-align">Email</label>
-		                    @error('email')
-	                                <span class="red-text" role="alert">
-	                                    <strong>{{ $message }}</strong>
-	                                </span>
-	                        @enderror
-		                </div>
-	                </div>
-
-	                <div class="row margin">
-		                <div class="input-field col s12">
-		                  <i class="material-icons prefix pt-2">lock_outline</i>
-		                  <input id="password" type="password" name="password">
-		                  <label for="password">Password</label>
-		                  	@error('password')
-	                                <span class="red-text" role="alert">
-	                                    <strong>{{ $message }}</strong>
-	                                </span>
-	                        @enderror
-		                </div>
-	                </div>
-
-	              	<div class="row">
-		                <div class="col s12 m12 l12 ml-2 mt-1">
-		                  <p>
-		                    <label>
-		                      <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}/>
-		                      <span>Remember Me</span>
-		                    </label>
-		                  </p>
-		                </div>
-	              	</div>
-
-	                <div class="row">
-		                <div class="input-field col s12">
-		                  <button type="submit" class="btn waves-effect waves-light border-round gradient-45deg-purple-deep-orange col s12">Login</button>
-		                </div>
-		            </div>
-
-	              <div class="row">
-	                <div class="input-field col s6 m6 l6">
-	                  <p class="margin medium-small"><a href="{{ route('register') }}">Register Now!</a></p>
-	                </div>
-
-	                @if (Route::has('password.request'))
-
-                        <div class="input-field col s6 m6 l6">
-		                  <p class="margin right-align medium-small"><a href="{{ route('password.request') }}">Forgot password ?</a></p>
-		                </div>
-
-                    @endif
-
-	                
-	              </div>
-	            </form>
-	          </div>
-	        </div>
-	        </div>
-	      </div>
-	    </div>
+    </div>
 
 	@endsection
 
--- resources/views/includes/auth/head.blade.php
+++ resources/views/includes/auth/head.blade.php
@@ -1,15 +1,11 @@
 	<title>@yield('title')</title>
     <link rel="apple-touch-icon" href="{{ asset('assets/admin/images/favicon/apple-touch-icon-152x152.png') }}">
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/admin/images/favicon/favicon-32x32.png') }}">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <!-- BEGIN: VENDOR CSS-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/admin/vendors/vendors.min.css') }}">
-    <!-- END: VENDOR CSS-->
-    <!-- BEGIN: Page Level CSS-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/admin/css/themes/vertical-modern-menu-template/materialize.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/admin/css/themes/vertical-modern-menu-template/style.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/admin/css/pages/dashboard.css') }}">
-    <!-- END: Page Level CSS-->
-    <!-- BEGIN: Custom CSS-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/admin/css/custom/custom.css') }}">
-    <!-- END: Custom CSS-->
+  
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/css/register.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/css/multi_form.css') }}">
+		<link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/css/login_register_common.css') }}">
+
+  
