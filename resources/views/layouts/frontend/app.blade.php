c6b672231d9a97f9a09c0661d7470dbe70a04adc
Asad Malik <74582564+asadmalik287@users.noreply.github.com>
2021-02-26 15:25:58 +0500

    chnages

--- resources/views/layouts/frontend/app.blade.php
+++ resources/views/layouts/frontend/app.blade.php
@@ -9,15 +9,10 @@
 	
 	@yield('footer')
 
-	<!-- BEGIN VENDOR JS-->
-    <script src="{{ asset('assets/admin/js/vendors.min.js') }}" type="text/javascript"></script>
-    <!-- BEGIN VENDOR JS-->
-
-    <!-- BEGIN THEME  JS-->
-    <script src="{{ asset('assets/admin/js/plugins.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('assets/admin/js/custom/custom-script.js') }}" type="text/javascript"></script>
-    <script type="text/javascript" src="{{ asset('assets/admin/js/scripts/ui-alerts.js') }}"></script>
-    <!-- END THEME  JS-->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 	@yield('extra-script')
 </body>
