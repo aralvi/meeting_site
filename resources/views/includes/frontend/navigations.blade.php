2c98bafe39193fad9a46ee9bd58604cc5f86d330
abdu rehman <arehmanewd@gmail.com>
2021-03-11 17:26:48 +0500

    cartegoires-display-updated

--- resources/views/includes/frontend/navigations.blade.php
+++ resources/views/includes/frontend/navigations.blade.php
@@ -1,7 +1,7 @@
 @if(count(categories()) > 0)
     <div class="pt-4 main_padding d-flex f-20 cl-3b3b3b3 justify-content-between robotoRegular">
         <div class="col-md-1 p-0"></div>
-        <ul class="listStyle-none p-0 col-md-10 d-flex robotoRegular f-18 ul_main_tabs m-0">
+        <ul class="listStyle-none p-0 col-md-10 d-flex robotoRegular f-18 ul_main_tabs m-0 d-flex justify-content-around">
             @foreach (categories() as $category)
                 <li class="pl-3"> <a href="#" class="cl-3b3b3b3">{{ ucwords($category->name) }}</a></li>
             @endforeach
