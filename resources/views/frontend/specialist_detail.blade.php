949b5b6640ea23a501b58132ce83e760c922e062
abdu rehman <arehmanewd@gmail.com>
2021-03-11 17:05:32 +0500

    number-format

--- resources/views/frontend/specialist_detail.blade.php
+++ resources/views/frontend/specialist_detail.blade.php
@@ -381,7 +381,7 @@
                     @endphp 
                     <td>{{ implode(',',array_map('ucwords',$subcategories)) }}</td>
                     <td>{{ $service->timing }} Minutes</td>
-                    <td> ${{number_format($service->rate) }} (USD)</td>
+                    <td> ${{number_format(intval($service->rate)) }} (USD)</td>
                     <td>{{ $service->status }}</td>
                     <td><a href="{{ route('appointment_request',encrypt($service->id)) }}" class="btn btn-outline-success my-2 my-sm-0 cl-ffffff bg-3ac574  pl-5 pr-5 login_button" >Book</a></td>
                   </tr>
