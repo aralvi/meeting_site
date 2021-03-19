b5c8bd559c17d65276f56c27f7a1e04e2dc219ae
Asad Malik <74582564+asadmalik287@users.noreply.github.com>
2021-03-19 17:32:15 +0500

    chnages

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -101,7 +101,7 @@
                     <!-- Example split danger button -->
                 <div class="btn-group">
                 <a href="" class=" dropdown-toggle dropdown-toggle-split cl-3b3b3b3"  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">More...</a>
-                <div class="dropdown-menu">
+                <div class="dropdown-menu dropdown-menu-nav">
                     @foreach (categories()->skip(8) as $category)
                     <a class="dropdown-item " href="#">{{ ucwords($category->name) }}</a>
                         
