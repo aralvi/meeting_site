ba01a002696b1e2868ee419a061d8d4b6a5356f6
abdu rehman <arehmanewd@gmail.com>
2021-03-26 15:23:37 +0500

    bids-updated

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1782,83 +1782,83 @@ figure:hover img {
           });
       }
 
-$(document).ready(function() {
-	$(".gallery").magnificPopup({
-		delegate: "a",
-		type: "image",
-		tLoading: "Loading image #%curr%...",
-		mainClass: "mfp-img-mobile",
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true,
-			preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-		},
-		image: {
-			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-		}
-	});
-});
-
-
-
-
-
-$(function() {
-            $("#sortableImgThumbnailPreview").sortable({
-             connectWith: ".RearangeBox",
-            
-                
-              start: function( event, ui ) { 
-                   $(ui.item).addClass("dragElemThumbnail");
-                   ui.placeholder.height(ui.item.height());
-           
-               },
-                stop:function( event, ui ) { 
-                   $(ui.item).removeClass("dragElemThumbnail");
-               }
-            });
-            $("#sortableImgThumbnailPreview").disableSelection();
+    $(document).ready(function() {
+        $(".gallery").magnificPopup({
+            delegate: "a",
+            type: "image",
+            tLoading: "Loading image #%curr%...",
+            mainClass: "mfp-img-mobile",
+            gallery: {
+                enabled: true,
+                navigateByImgClick: true,
+                preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+            },
+            image: {
+                tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+            }
         });
+    });
 
 
 
 
-document.getElementById('files').addEventListener('change', handleFileSelect, false);
 
-  function handleFileSelect(evt) {
-    
-    var files = evt.target.files; 
-    var output = document.getElementById("sortableImgThumbnailPreview");
-    
-    // Loop through the FileList and render image files as thumbnails.
-    for (var i = 0, f; f = files[i]; i++) {
-
-      // Only process image files.
-      if (!f.type.match('image.*')) {
-        continue;
-      }
-
-      var reader = new FileReader();
-
-      // Closure to capture the file information.
-      reader.onload = (function(theFile) {
-        return function(e) {
-          // Render thumbnail.
-           var imgThumbnailElem = "<div class='RearangeBox imgThumbContainer'><i class='material-icons imgRemoveBtn' onclick='removeThumbnailIMG(this)'>cancel</i><div class='IMGthumbnail' ><img  src='" + e.target.result + "'" + "title='"+ theFile.name + "'/></div><div class='imgName'>"+ theFile.name +"</div></div>";
+    $(function() {
+                $("#sortableImgThumbnailPreview").sortable({
+                connectWith: ".RearangeBox",
+                
                     
-                    output.innerHTML = output.innerHTML + imgThumbnailElem; 
-          
-        };
-      })(f);
+                start: function( event, ui ) { 
+                    $(ui.item).addClass("dragElemThumbnail");
+                    ui.placeholder.height(ui.item.height());
+            
+                },
+                    stop:function( event, ui ) { 
+                    $(ui.item).removeClass("dragElemThumbnail");
+                }
+                });
+                $("#sortableImgThumbnailPreview").disableSelection();
+            });
 
-      // Read in the image file as a data URL.
-      reader.readAsDataURL(f);
+
+
+
+    document.getElementById('files').addEventListener('change', handleFileSelect, false);
+
+    function handleFileSelect(evt) {
+        
+        var files = evt.target.files; 
+        var output = document.getElementById("sortableImgThumbnailPreview");
+        
+        // Loop through the FileList and render image files as thumbnails.
+        for (var i = 0, f; f = files[i]; i++) {
+
+        // Only process image files.
+        if (!f.type.match('image.*')) {
+            continue;
+        }
+
+        var reader = new FileReader();
+
+        // Closure to capture the file information.
+        reader.onload = (function(theFile) {
+            return function(e) {
+            // Render thumbnail.
+            var imgThumbnailElem = "<div class='RearangeBox imgThumbContainer'><i class='material-icons imgRemoveBtn' onclick='removeThumbnailIMG(this)'>cancel</i><div class='IMGthumbnail' ><img  src='" + e.target.result + "'" + "title='"+ theFile.name + "'/></div><div class='imgName'>"+ theFile.name +"</div></div>";
+                        
+                        output.innerHTML = output.innerHTML + imgThumbnailElem; 
+            
+            };
+        })(f);
+
+        // Read in the image file as a data URL.
+        reader.readAsDataURL(f);
+        }
     }
-  }
 
-  function removeThumbnailIMG(elm){
-    elm.parentNode.outerHTML='';
-  }
+    function removeThumbnailIMG(elm){
+        elm.parentNode.outerHTML='';
+    }
 
 
 </script>
