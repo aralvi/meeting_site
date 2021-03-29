ce112694e26f59e1c7d2364ccf6cc13175db6c8d
abdu rehman <arehmanewd@gmail.com>
2021-03-29 14:28:19 +0500

    updated

--- resources/views/stripe.blade.php
+++ resources/views/stripe.blade.php
@@ -1,14 +1,139 @@
      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<style>
+    .loader11 {
+        width: 100px;
+        height: 70px;
+        margin: 50px auto;
+        position: relative
+    }
 
+    .loader11 span {
+        display: block;
+        width: 5px;
+        height: 10px;
+        background: #e43632;
+        position: absolute;
+        bottom: 0;
+        animation: loading-11 2.25s infinite ease-in-out
+    }
 
+    .loader11 span:nth-child(2) {
+        left: 11px;
+        animation-delay: .2s
+    }
 
+    .loader11 span:nth-child(3) {
+        left: 22px;
+        animation-delay: .4s
+    }
+
+    .loader11 span:nth-child(4) {
+        left: 33px;
+        animation-delay: .6s
+    }
+
+    .loader11 span:nth-child(5) {
+        left: 44px;
+        animation-delay: .8s
+    }
+
+    .loader11 span:nth-child(6) {
+        left: 55px;
+        animation-delay: 1s
+    }
+
+    .loader11 span:nth-child(7) {
+        left: 66px;
+        animation-delay: 1.2s
+    }
+
+    .loader11 span:nth-child(8) {
+        left: 77px;
+        animation-delay: 1.4s
+    }
+
+    .loader11 span:nth-child(9) {
+        left: 88px;
+        animation-delay: 1.6s
+    }
+
+    @-webkit-keyframes loading-11 {
+        0% {
+            height: 10px;
+            transform: translateY(0);
+            background: #ff4d80
+        }
+
+        25% {
+            height: 60px;
+            transform: translateY(15px);
+            background: #3423a6
+        }
+
+        50% {
+            height: 10px;
+            transform: translateY(-10px);
+            background: #e29013
+        }
+
+        100% {
+            height: 10px;
+            transform: translateY(0);
+            background: #e50926
+        }
+    }
+
+    @keyframes loading-11 {
+        0% {
+            height: 10px;
+            transform: translateY(0);
+            background: #ff4d80
+        }
+
+        25% {
+            height: 60px;
+            transform: translateY(15px);
+            background: #3423a6
+        }
+
+        50% {
+            height: 10px;
+            transform: translateY(-10px);
+            background: #e29013
+        }
+
+        100% {
+            height: 10px;
+            transform: translateY(0);
+            background: #e50926
+        }
+    }
+
+</style>
+
+<div class="row afterRegisterLoader" style="display: none;">
+                    <div class="col-md-12">
+                        <div class="loader11">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
+                </div>
 <form role="form" action="{{ route('stripe.post') }}" method="post" class="require-validation" data-cc-on-file="false"
     data-stripe-publishable-key="{{ $specialist->stripe_public_key }}" id="payment-form">
     @csrf
     <input type="hidden" name="stripe_public_key" value="{{ $specialist->stripe_public_key }}">
     <div class=' row'>
         <div class='col-md-12 form-group required'>
-            <label class='control-label'>Name on Card</label> <input class='form-control border-bottom' size='4' type='text' placeholder="ex. ewdtech">
+            <label class='control-label'>Name on Card</label> <input class='form-control border-bottom text-capitalize' size='4' type='text' placeholder="ex. ewdtech">
         </div>
     </div>
 
@@ -19,7 +144,7 @@
         </div>
         <div class='col-md-6 form-group  required'>
             <label class='control-label'>Amount</label> <input autocomplete='off' class='form-control border-bottom ' name="amount"
-                type='number' value="{{ $amount }}" placeholder="ex. $100 (USD)">
+                type='number' value="{{ $amount }}" placeholder="ex. $100 (USD)" min="0">
         </div>
     </div>
 
@@ -30,11 +155,11 @@
         </div>
         <div class='col-sm-12 col-md-4 form-group expiration required'>
             <label class='control-label'>Expiration Month</label> <input class='form-control border-bottom card-expiry-month'
-                placeholder='MM' size='2' type='text'>
+                placeholder='MM'  min="0" type='number'>
         </div>
         <div class='col-sm-12 col-md-4 form-group expiration required'>
             <label class='control-label'>Expiration Year</label> <input class='form-control border-bottom card-expiry-year'
-                placeholder='YYYY' size='4' type='text'>
+                placeholder='YYYY'  min="0" type='number'>
         </div>
     </div>
 
@@ -105,6 +230,7 @@
                 // insert the token into the form so it gets submitted to the server
                 $form.find('input[type=text]').empty();
                 $form.append("<input type='hidden' name='stripeToken' value='" + token + "'/>");
+                $('.afterRegisterLoader').show();
                 $.ajax({
                     type: 'post',
                     url: "{{ route('stripe.post') }}",
@@ -115,6 +241,9 @@
                             text: "{{ __('Payment submit successfully') }}",
                             icon: 'success'
                         });
+                        $('.afterRegisterLoader').hide();
+                        $("#payment_modal .close").click() 
+
                     }, 
 
                 })
