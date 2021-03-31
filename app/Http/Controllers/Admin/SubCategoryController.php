54c7a5a87d24189804805f610a7008bbc4e49de5
MuhammadWaseem961 <60288560+MuhammadWaseem961@users.noreply.github.com>
2021-03-31 15:59:20 +0500

    partial

--- app/Http/Controllers/Admin/SubCategoryController.php
+++ app/Http/Controllers/Admin/SubCategoryController.php
@@ -1,7 +1,7 @@
 <?php
 
-namespace App\Http\Controllers;
-
+namespace App\Http\Controllers\Admin;
+use App\Http\Controllers\Controller;
 use App\SubCategory;
 use Illuminate\Http\Request;
 
@@ -24,7 +24,7 @@ class SubCategoryController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.subcategories.create');
     }
 
     /**
