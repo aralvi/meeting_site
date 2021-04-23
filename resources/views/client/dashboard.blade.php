b6c41a29a5780572953f541507bd00f685da9252
MuhammadWaseem961 <60288560+MuhammadWaseem961@users.noreply.github.com>
2021-04-23 17:00:46 +0500

    updated

--- resources/views/client/dashboard.blade.php
+++ resources/views/client/dashboard.blade.php
@@ -692,7 +692,14 @@ span.prefix{
                 </div>
             </div>
         </section>
-
+    @else
+        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            <strong>Your Profile is under view and administrator will approved your profile as soon as possible
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        
     @endif
 
     
