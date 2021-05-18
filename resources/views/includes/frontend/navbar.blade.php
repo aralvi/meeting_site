0a1df68e7e9c36a02c0160ddc10baad9f91d69d9
Asad Malik <74582564+asadmalik287@users.noreply.github.com>
2021-05-18 15:49:12 +0500

    video-updated

--- resources/views/includes/frontend/navbar.blade.php
+++ resources/views/includes/frontend/navbar.blade.php
@@ -210,6 +210,7 @@
                             url: '{{ url("call-checker") }}',
                             data: { name: username },
                             success: function(data) {
+                                alert(data.check)
                                 if(data.status == 'success' && data.caller !='{{Auth::user()->username}}' && data.call_to == '{{Auth::user()->username}}'  ){
                                     $('.calling-div').removeClass('d-none');
                                     $('.incoming-call').html('Incoming call from '+data.caller[0].toUpperCase()+data.caller.slice(1));
